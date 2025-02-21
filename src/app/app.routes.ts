18c9809d670e66ebfb71ca9600737e5e96878e8a
AbubakarFarooqi <azanalifarooqi@gmail.com>
2025-02-21 06:55:03 +0500

    fix router link

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [{ path: '', component: HomeComponent },];
+export const routes: Routes = [{ path: '', component: HomeComponent }];
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {
-
-}
+export class NavbarComponent {}
