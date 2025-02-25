2e0f9421bc37eff1d80f1b2f4760f1c8096afe59
ibbitiminecraft <khadijakanwal90@gmail.com>
2025-02-24 19:56:19 -0600

    css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
+import { MebersComponent } from './components/mebers/mebers.component';
+
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
---
+++ src/app/components/mebers/mebers.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mebers',
+  imports: [],
+  templateUrl: './mebers.component.html',
+  styleUrl: './mebers.component.css'
+})
+export class MebersComponent {
+
+}
