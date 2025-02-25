9246a0f496a4c4b4512349b6556d075778f2c30c
AbubakarFarooqi <azanalifarooqi@gmail.com>
2025-02-25 06:43:08 +0500

    add css variables and fix routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,4 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 
-export const routes: Routes = [{ path: '', component: HomeComponent},{ path: 'about', component: AboutComponent}];
+export const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'about', component: AboutComponent },
+];
