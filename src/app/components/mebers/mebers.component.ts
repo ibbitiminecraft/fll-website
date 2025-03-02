aab3e2c3dfe7ac8ad96b2fe6a3960349bbd28a3e
AbubakarFarooqi <azanalifarooqi@gmail.com>
2025-03-03 04:15:55 +0500

    Update member and footer comoponent

--- src/app/components/mebers/mebers.component.ts
+++ src/app/components/mebers/mebers.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { TeamMemberService } from '../../services/team-member.service';
 import { TeamMemberModel } from '../../services/team-member.model';
 import { TemplateBindingParseResult } from '@angular/compiler';
-import { NgStyle } from '@angular/common';
+import { NgFor, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-mebers',
-  imports: [NgStyle],
+  imports: [NgStyle, NgFor],
   templateUrl: './mebers.component.html',
   styleUrl: './mebers.component.css',
 })
