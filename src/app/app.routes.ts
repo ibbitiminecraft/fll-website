06572d5b4de7b02afadfad3fe40a1ef125f164ac
ibbitiminecraft <khadijakanwal90@gmail.com>
2025-03-04 19:56:55 -0600

    vidios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { AboutComponent } from './components/about/about.component';
 import { MebersComponent } from './components/mebers/mebers.component';
 import { CoreCaluesComponent } from './components/core-calues/core-calues.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { MediaGallaryComponent } from './components/media-gallary/media-gallary.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
@@ -11,4 +12,6 @@ export const routes: Routes = [
   { path: 'members', component: MebersComponent },
   { path: 'core-calues', component: CoreCaluesComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'core-calues', component: CoreCaluesComponent },
+  { path: 'media-gallary', component: MediaGallaryComponent },
 ];
---
+++ src/app/components/media-gallary/media-gallary.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-media-gallary',
+  imports: [],
+  templateUrl: './media-gallary.component.html',
+  styleUrl: './media-gallary.component.css'
+})
+export class MediaGallaryComponent {
+
+}
